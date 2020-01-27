827dc40bb6cec6a0c5e0b3e659124aff303a90e4
Alex <dnjscksdn98@gmail.com>
2020-01-27 16:23:13 +0900

    useSelector 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,6 @@
 // Container Component : 리덕스에 있는 상태를 조회하거나 액션을 디스패치 할 수 있는 컴포넌트
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 
 import Counter from "../components/Counter";
 import { increase, decrease, setDiff } from "../modules/counter";
@@ -8,10 +8,13 @@ import { increase, decrease, setDiff } from "../modules/counter";
 function CounterContainer() {
   // state : store.getStore() 사용 시 반환되는 객체를 가리킴 - 리덕스의 현재 상태
   // useSelector: 상태 조회
-  const { number, diff } = useSelector(state => ({
-    number: state.counter.number,
-    diff: state.counter.diff
-  }));
+  const { number, diff } = useSelector(
+    state => ({
+      number: state.counter.number,
+      diff: state.counter.diff
+    }),
+    shallowEqual
+  );
 
   // useDispatch: 디스패치 생성
   const dispatch = useDispatch();
